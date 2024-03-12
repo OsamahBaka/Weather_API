dcf19c2e13e31a2d5e58dacd8b64e058beb61bfe
OsamahBaka <osamahkhaled05@gmail.com>
2024-03-12 14:06:07 +0300

    adding swagger file

--- Weather_API/urls.py
+++ Weather_API/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from weather.views import swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('weather/swagger/', swagger, name='swagger'),
     path('weather/', include('weather.urls')),
 ]
