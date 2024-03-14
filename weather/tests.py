3fb05920bae7fb11c26b66e8d3998e78ad9d8007
OsamahBaka <osamahkhaled05@gmail.com>
2024-03-14 12:05:00 +0300

    add websocket using channels and rout it with wether/live/

--- weather/tests.py
+++ weather/tests.py
@@ -8,17 +8,14 @@ class WeatherByCityViewTests(TestCase):
         self.client = APIClient()
 
     def test_success_response(self):
-        # Test success response when city exists
         response = self.client.get('/weather/Jeddah/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_fail_response(self):
-        # Test failure response when city parameter is not provided
         response = self.client.get('/weather/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
     
     def test_fail_response2(self):
-        # Test failure response when the API request returns an error
         response = self.client.get('/weather/NonexistentCity/')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -28,13 +25,11 @@ class WeatherBulkViewTests(TestCase):
         self.client = APIClient()
 
     def test_success_response(self):
-        # Test success response when providing valid city list
         data = {"cities": ["Jeddah", "Makkah", "Riyadh"]}
         response = self.client.post('/weather/bulk/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_fail_response(self):
-        # Test failure response when not providing city list
         response = self.client.post('/weather/bulk/')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -44,6 +39,5 @@ class WeatherStatisticsViewTests(TestCase):
         self.client = APIClient()
 
     def test_success_response(self):
-        # Test success response when weather data exists
         response = self.client.get('/weather/statistics/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
